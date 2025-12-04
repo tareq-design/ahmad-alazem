9b96a0437c425336fe24fa254214820cf72775bc
tareq-osama <tarekaless99@gmail.com>
2025-12-04 06:06:23 +0300

    Localization init

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -116,7 +116,7 @@ export interface Config {
     header: HeaderSelect<false> | HeaderSelect<true>;
     footer: FooterSelect<false> | FooterSelect<true>;
   };
-  locale: null;
+  locale: 'en' | 'ar';
   user: User & {
     collection: 'users';
   };
